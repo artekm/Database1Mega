6071ed5446962dc32de134503be5c81833bb8a22
artekm <44265668+artekm@users.noreply.github.com>
2019-11-20 19:02:46 +0100

    Homework created

---
+++ src/main/java/artur/database/DatabaseApplication.java
@@ -0,0 +1,44 @@
+package artur.database;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Slf4j
+@SpringBootApplication
+public class DatabaseApplication implements CommandLineRunner {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private MyRepository repository;
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabaseApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        PrepareDatabase();
+
+        int[] table = new int[1_000_000];
+        Arrays.setAll(table, i -> i);
+        List<Person> result = repository.getPeopleWithID(table);
+        log.info(result.toString());
+    }
+
+    private void PrepareDatabase() {
+        jdbcTemplate
+                .execute("create table person ( id integer not null, first_name varchar(255) not null, last_name varchar(255) not null, primary key(id));");
+        jdbcTemplate
+                .execute("insert into person  values (1,'John','Doe'),(2,'Jane','Doe'),(300,'Anna','Frank'),(400,'Anna','Noname');");
+    }
+}
+
